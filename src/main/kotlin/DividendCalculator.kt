573eaea118a4bb5cf18ff4f4da37dd203e919db9
toboche <toboche@gmail.com>
2021-12-26 21:39:45 +0100

    dividend calculating

--- src/main/kotlin/DividendCalculator.kt
+++ src/main/kotlin/DividendCalculator.kt
@@ -11,10 +11,11 @@ class DividendCalculator {
         .filter { dateRange.contains(it.date) }
         .sumOf {
             //                12.55*1/(1-0.15)
-            val alreadyPaidTaxInUsa =
-                it.totalAmount * BigDecimal.ONE * (BigDecimal.ONE - dividendTaxRatePercentAlreadyPaidInUsa)
-            val totalDividendTax = it.totalAmount * totalDividendTaxRatePercent
-            totalDividendTax - alreadyPaidTaxInUsa
+            val grossDividend =
+                it.totalAmount * BigDecimal.ONE / (BigDecimal.ONE - dividendTaxRatePercentAlreadyPaidInUsa)
+            val alreadyPaidTaxInUsa = grossDividend - it.totalAmount
+            val totalDividendTax = grossDividend * totalDividendTaxRatePercent
+            (totalDividendTax - alreadyPaidTaxInUsa)
         }
 
 }
