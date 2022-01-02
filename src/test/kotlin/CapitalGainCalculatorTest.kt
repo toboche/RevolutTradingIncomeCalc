103f741e2b8f3686d63d7555205335363f91b010
toboche <toboche@gmail.com>
2022-01-02 11:20:29 +0100

    this year's tax calculated, including the USD-PLN conversion

--- src/test/kotlin/CapitalGainCalculatorTest.kt
+++ src/test/kotlin/CapitalGainCalculatorTest.kt
@@ -20,5 +20,12 @@ internal class CapitalGainCalculatorTest {
             Offset.offset(BigDecimal("0.01"))
         )
         assertThat(result.finalLoss).isEqualTo(ZERO)
+
+//        result = {CapitalGainCalculator$GainAndExpenses@3287} GainAndExpenses(dividendTaxLeftToPay=20.58496419, custodyFees=71.505504, tickerGainCalculationResult=GainTax(tax=328.9150968946459176), finallyToPay=277.9945570846459176, finalLoss=0)
+//        dividendTaxLeftToPay = {BigDecimal@3547} "20.58496419"
+//        custodyFees = {BigDecimal@3548} "71.505504"
+//        tickerGainCalculationResult = {TickerBalanceCalculator$GainTax@3549} GainTax(tax=328.9150968946459176)
+//        finallyToPay = {BigDecimal@3550} "277.9945570846459176"
+//        finalLoss = {BigDecimal@3551} "0"
     }
 }
